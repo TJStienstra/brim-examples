71db4bbf5079f0f9ccd15d67f70c2142f5630af6
TJStienstra <timostienstra00@gmail.com>
2023-08-15 14:40:18 +0200

    Remove timer

--- examples/whipple_upper_rider_simulation/simulate.py
+++ examples/whipple_upper_rider_simulation/simulate.py
@@ -1,6 +1,4 @@
 """Forward dynamics simulation of the Whipple bicycle with an upper body rider model."""
-import time
-
 import cloudpickle
 import numpy as np
 
@@ -17,13 +15,11 @@ if "simulator" in data and True:
     simulator.initial_conditions = data.initial_conditions
     simulator.controls = data.controls
 else:
-    start = time.time()
     simulator = Simulator(data.system)
     simulator.constants = data.constants
     simulator.initial_conditions = data.initial_conditions
     simulator.controls = data.controls
     simulator.initialize(check_parameters=False)
-    end = time.time()
 
     data.simulator = simulator
     with open(filename, "wb") as f:
@@ -34,9 +30,7 @@ try:
     simulator.solve(time_arr, solver="dae", rtol=1e-3, atol=1e-6)
     print("Simulated with DAE solver")  # noqa: T201
 except ImportError:
-    start = time.time()
     simulator.solve((0, time_arr[-1]), t_eval=time_arr)
-    end = time.time()
     print("Simulated with ODE solver")  # noqa: T201
 
 data.simulator = simulator
